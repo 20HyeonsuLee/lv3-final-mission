51c428c978438b01a0145ca56f40455e36aa8718
HyeonsuLee <leehyeonsu4888@naver.com>
2025-06-10 16:24:41 +0900

    refactor: 컨트롤러 책임 변경

---
+++ src/main/java/finalmission/member/controller/MemberController.java
@@ -0,0 +1,26 @@
+package finalmission.member.controller;
+
+import finalmission.member.controller.dto.LoginRequest;
+import finalmission.member.model.member.Member;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody @Valid final LoginRequest request, final HttpSession session) {
+        Member member = memberService.login(request);
+        session.setAttribute("LOGIN_MEMBER", member);
+        return ResponseEntity.ok().build();
+    }
+}
